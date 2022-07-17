0a6d28b60390e01440c0a8c55651662a8764d90d
nahmad04 <109441757+nahmad04@users.noreply.github.com>
2022-07-16 21:59:45 -0400

    Add files via upload

---
+++ Beginners/dataTypeOverflow.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int intMax = INT_MAX;
+    cout << intMax << endl;
+    cout << intMax + 1;
+    //Goes from max value to min value just by adding one, works like a clock
+    //Clock goes 10, 11, 12, the max value, and then goes to 1, the min value
+    
+    return 0;
+}
